698089ba4c68aa9b7570e30f53cf33fa345d5dfb
id2654-ensai <gabrielle.yanni@eleve.ensai.fr>
2025-09-12 14:14:29 +0000

    travail tp1

---
+++ src/business_object/pokemon/defender_pokemon.py
@@ -0,0 +1,10 @@
+from business_object.pokemon.abstract_pokemon import AbstractPokemon
+
+
+class DefenderPokemon(AbstractPokemon):
+    def get_pokemon_attack_coef(self) -> float:
+        """
+        Commentaire
+        """
+        multiplier = 1 + (self.attack_current + self.defense_current) / 200
+        return multiplier
